ed8d122d8f3bd45e9eb06e945929647290cfac05
Taemin Cha <krlrhkstk12@gmail.com>
2021-08-07 21:45:56 +0900

    Show usage if don't know mode

--- main.go
+++ main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"flag"
-	"log"
 )
 
 func main() {
 	mode := flag.String("mode", "", "server or client")
-	initialCapacity := flag.Int("capacity", 4096000, "initial memory allocation capacity")
-	workers := flag.Int("workers", 32, "worker pool size")
-	key := flag.String("key", "", "key string")
-	value := flag.String("value", "", "value string")
-	command := flag.String("command", "", "[get|set|remove]")
-	n := flag.Int("n", 0, "benchmark running count")
-	parallel := flag.Int("parallel", 0, "benchmark parallelism")
-	keySize := flag.Int("key-size", 128, "benchmark key size")
+	initialCapacity := flag.Int("capacity", 4096000, "(server) initial memory allocation capacity")
+	workers := flag.Int("workers", 32, "(server) worker pool size")
+	key := flag.String("key", "", "(client) key string")
+	value := flag.String("value", "", "(client) value string")
+	command := flag.String("command", "", "(client) [get|set|remove|repl]")
+	n := flag.Int("n", 0, "(benchmark) benchmark running count")
+	parallel := flag.Int("parallel", 0, "(benchmark) benchmark parallelism")
+	keySize := flag.Int("key-size", 128, "(benchmark) benchmark key size")
 	flag.Parse()
 
 	switch *mode {
@@ -23,6 +22,6 @@ func main() {
 	case "client":
 		client(command, key, value, *n, *parallel, *keySize)
 	default:
-		log.Panicln("[ERROR] don't know how to run", mode)
+		flag.Usage()
 	}
 }
